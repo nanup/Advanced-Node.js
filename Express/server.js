beb1006d844eaf7842f406a476f4ca5118478d25
nanup <nanup.personal+github@gmail.com>
2023-08-02 11:17:30 +0530

    friendById

--- Express/server.js
+++ Express/server.js
@@ -4,8 +4,37 @@ const app = express();
 
 const PORT = 3000;
 
+const friends = [
+  {
+    id: 0,
+    name: 'Nanu Panchamurthy',
+  },
+  {
+    id: 1,
+    name: 'Anil Kumar Pappu',
+  },
+  {
+    id: 2,
+    name: 'Akhil Kumar Tangi',
+  },
+];
+
 app.get('/', (req, res) => {
   res.send('Hello!');
+});
+
+app.get('/friends', (req, res) => {
+  res.json(friends);
+});
+
+app.get('/friends/:id', (req, res) => {
+  if (+req.params.id < friends.length) {
+    res.json(friends[+req.params.id]);
+  } else {
+    res.sendStatus(404).json({
+      error: "Friend doesn't exist.",
+    });
+  }
 });
 
 app.get('/messages', (req, res) => {
